75feef5b101fb04d1ca28b8a6db504be7923d27d
Mauricio <soles369@yahoo.com.br>
2016-07-20 10:39:23 +0000

    instrucao

---
+++ mascara.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+//Metodo de retorno da operacao utilizada
+[4]char operacao (int instruction){
+
+
+}
+//retorna a instrução
+int instrucao(int wholeWorld){
+	instruction=(0x68000004 & 0xFC000000)>>26;
+	return instruction
+}
+int main(){
+		
+	printf("%x \n", a);
+	return 0;
+}
