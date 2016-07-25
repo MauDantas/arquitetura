a6fa5f60ced065ba6caf1a9ff716c7ec65ad6f11
MauDantas <soles369@yahoo.com.br>
2016-07-25 10:25:34 -0300

    Update mascara.c

--- mascara.c
+++ mascara.c
@@ -2,24 +2,58 @@
 
 //Metodo de retorno da operacao utilizada
 
-/*Intrucoes de impressao
-unsigned int a=0;
-	int b=0;
-	a=a-1;
-	b=b-1;
-	printf("A=%i, B=%u \n", a, b);
-	printf("A=0x%08x, B=0x%08x \n", a, b);
-*/
+
 char* operacao (int instruction){
-
-
+	/*switch(instruction)
+		case 0:
+		break
+		case 1:
+		break
+		case 2:
+		break
+		case 3:
+		break
+		case 4:
+		break
+		case 5:
+		break
+		case 6:
+		break
+		case 7:
+		break
+		case 8:
+		break
+		case 9:
+		break
+		case 10:
+		break
+		case 11:
+		break
+		case 12:
+		break
+		case 13:
+		break
+		case 14:
+		break
+		case 15:
+		break
+		case 16:
+		break
+		case 17:
+		break
+		case 18:
+		break
+		case 19:
+		break
+		case 20:
+		break
+*/
 }
 //retorna a instrução
 int instrucao(int wholeWorld){
-	int instruction=(0x68000004 & 0xFC000000)>>26;
+	int instruction=(wholeWorld & 0xFC000000)>>26;
 	return instruction;
 }
 int main(){
 	
-	return 0;
 }
