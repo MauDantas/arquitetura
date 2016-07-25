76f22ef39ca700fc5be17356871df2d64b70028a
MauDantas <soles369@yahoo.com.br>
2016-07-25 10:24:51 -0300

    Create anotacoes.c

---
+++ anotacoes.c
@@ -0,0 +1,22 @@
+/*Sinal é mera convenção
+	unsigned int a=0;
+	int b=0;
+	a=a-1;
+	b=b-1;
+	printf("A=%i, B=%u \n", a, b);
+	printf("A=0x%08x, B=0x%08x \n", a, b);
+*/
+/*
+Ponto fixo 
+	unsigned int a,b;
+	a= 0x4A000000;
+	b=0x48E00000;
+	printf("R= 0x%08X \n", a+b);
+	
+*/
+/*
+float  a=9.25;
+	unsigned long* b=(unsigned long*)(&a);
+	printf ("A= 0x%08x \n", *b);
+	return 0;
+*/
