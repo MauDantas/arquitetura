f1f758552a2d99eb411b573801ff1353e98a9c92
Ma <soles369@yahoo.com.br>
2016-08-12 18:15:57 -0300

    preparando impressao

--- imprimindo.c
+++ imprimindo.c
@@ -12,13 +12,15 @@ char flag1=0;
 	 34 ER - Registrador de extensao
 	 35 FR - Dados de comparacoes e info do processador*/
 long final=0;
-//R0 sempre zero
+//todos registradores são zerados
 void cleanR(){
 	char i;
-	for(i=0; i<35; i++){
+	for(i=0; i<36; i++){
 		R[i]=0;
 	}
 }
+//R0 sempre zero
+
 void persistR0(){
 	R[0]=0;}
 //Metodo de retorno da operacao utilizada
@@ -152,9 +154,11 @@ long opS(long wholeWord){
 }
 
 //modificacoes feitas por instrucoes tipo U
-void RspU(unsigned long instruction, unsigned long Rx, unsigned long Ry, unsigned long Rz){
+void RspU(unsigned long instruction, unsigned long Rz, unsigned long Rx, unsigned long Ry){
 	unsigned long aux1, aux2;
-	long long aux3, aux4, aux5;
+	long aux3;
+	long aux4;
+	long aux5;
 	switch (instruction){
 		//add
 		case 0:
@@ -182,39 +186,40 @@ void RspU(unsigned long instruction, unsigned long Rx, unsigned long Ry, unsigne
 		break;
 		//mul
 		case 4:
-			if((Rx == flag)){
+			if((Rx == flag1)){
 				aux3=((R[34]<<32)+R[Rx])*R[Ry];
 				aux4=(0x00000000FFFFFFFF)&aux3;
 				R[Rz]=aux4;
 				R[34]=(0xFFFFFFFF00000000&aux3)>>32;
-				printf ("%d*%d=%d\n",R[Rx],R[Ry],R[Rz]);
+				//printf ("%d*%d=%d\n",R[Rx],R[Ry],R[Rz]);
 				if(R[34]!=0){
 					if(R[35]<0x10)
 						R[35]=R[35]+0x00000010;	
 				}
-				flag=Rz;
+				flag1=Rz;
 			}
-			else if((Ry == flag)){
+			else if((Ry == flag1)){
 				aux3=((R[34]<<32)+R[Ry])*R[Rx];
 				R[Rz]= 0x00000000FFFFFFFF&aux3;
 				R[34]=(0xFFFFFFFF00000000&aux3)>>32;
-				printf ("%d*%d=%d\n",R[Rx],R[Ry],R[Rz]);
+				//printf ("%d*%d=%d\n",R[Rx],R[Ry],R[Rz]);
 				if(R[34]!=0){
 					if(R[35]<0x10)
 						R[35]=R[35]+0x00000010;	
 				}
-				flag=Rz;
+				flag1=Rz;
 			}
 			else{
 				R[34]=0;
 				aux3=R[Rx]*R[Ry];
 				R[Rz]=0x00000000FFFFFFFF&aux3;
 				R[34]=(0xFFFFFFFF00000000&aux3)>>32;
-				printf ("%d*%d=%d\n",R[Rx],R[Ry],R[Rz]);
+				//printf ("%d*%d=%d\n",R[Rx],R[Ry],R[Rz]);
 				if(R[34]!=0){
 					if(R[35]<0x10)
 						R[35]=R[35]+0x00000010;	
 				}
+				flag1=Rz;
 			}
 			R[32]++;
 		break;
@@ -252,22 +257,28 @@ void RspU(unsigned long instruction, unsigned long Rx, unsigned long Ry, unsigne
 		break;
 		//shl
 		case 10:
-			if(flag == Rx && Rx !=0){
-				aux3=((R[34]<<32)+R[Rx])<<(Ry+1);
-				R[Rz]=(0x00000000FFFFFFFF&aux3);
-				R[34]=(0xFFFFFFFF00000000&aux3)>>32;
+			//printf("The flag is %d ",flag1);
+			if(Rz==0){
+				R[34]=R[Rx]<<(Ry+1);
+			}
+			else if(flag1 == Rx){
+				printf("Rz = R%d. R%d=%X Ry=%d ",Rz,Rx,R[Rx],Ry);
+				R[34]=(R[34]<<(Ry+1))+(R[Rx]>>(32-(Ry+1)));
+				R[R[Rz]]=R[Rx]<<(Ry+1);
+				//printf ("Er=%X Rz=%X \n", R[34], R[Rz]);
 			}
 			else{
-				aux3=R[Rx]<<(Ry+1);
-				R[Rz]=(0x00000000FFFFFFFF&aux3);
-				printf("stoy aqui, querendo te \n");
-				R[34]=(0xFFFFFFFF00000000&aux3)>>32;
+				//printf("Rz = R%d. R%d=%X Ry=%d ",Rz,Rx,R[Rx],Ry);
+				R[34]=R[Rx]>>(32-(Ry+1));
+				R[Rz]=R[Rx]<<(Ry+1);
+				//printf ("Er=%X Rz=%X \n", R[34], R[Rz]);
+				flag1=Rz;
 			}
 			R[32]++;
 		break;
 		//shr
 		case 11:
-			if(flag == Rx){
+			if(flag1 == Rx){
 				aux3=((R[34]<<32)+R[Rx])>>(Ry+1);
 				R[Rz]=(0x00000000FFFFFFFF&aux3);
 				R[34]=(0xFFFFFFFF00000000&aux3)>>32;
@@ -300,7 +311,6 @@ void RspU(unsigned long instruction, unsigned long Rx, unsigned long Ry, unsigne
 			R[32]++;
 		break;		
 		}
-	persistR0();
 }
 
 //modificacoes feitas por instrucoes tipo F
@@ -313,7 +323,7 @@ void RspF(unsigned long instruction, unsigned long IM16, unsigned long Rx, unsig
 			aux1=R[Ry];
 			aux2=IM16;
 			R[Rx]=IM16+R[Ry];
-			printf("%d+%d=%d\n", aux1, IM16,R[Rx]);
+			//printf("%d+%d=%d\n", aux1, IM16,R[Rx]);
 			if((aux1+aux2)>0xFFFFFFFF||(R[Rx]+R[Ry])>0x7FFFFFFF)
 			{
 				if(R[35]<0x10)
@@ -502,7 +512,8 @@ int main(){
 		else if(parse[4] == 85){ //U U0=E U1=Rz	U2=Rx U3=Ry;
 				U=opU(wholeWord[R[32]]);
 				RspU(instruction[R[32]], U[1], U[2], U[3]);
-				printf("%d %s saida: %X Rz: %X R%d: %X R%X: %X \n", i,parse,R[U[1]],U[1],U[2], R[U[2]],U[3],R[U[3]]);
+				persistR0();
+				printf("%d %s saida: %X Rz: %X R%d: %X R%d: %X ER=%X \n", i,parse,R[U[1]],U[1],U[2], R[U[2]],U[3],R[U[3]],R[34]);
 				i++;
 		}
 		else if(parse[4] == 70){ //F f0=IM16 F1=Rx F2=Ry
@@ -585,7 +596,7 @@ int main(){
 						}
 				R[32]++;	
 				}
-				printf("%d %s saida: %X Rx: %X Ry: %X IM16 %X \n", i,parse,R[F[1]],R[F[1]],R[F[2]], F[0]);
+				printf("%d %s saida: %X R%d: %X R%d: %X IM16 %X \n", i,parse,R[F[1]],F[1],R[F[1]],F[2],R[F[2]], F[0]);
 				i++;
 		}
 		else if(parse[4] == 83){ //S
